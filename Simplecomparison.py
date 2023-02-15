2cff24e7f89f1f90d0f8e1fff7704ad95a35bd71
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 13:44:50 -0600

    false true

--- Simplecomparison.py
+++ Simplecomparison.py
@@ -1,4 +1,4 @@
 n=int(input("enter a number"))
-print(n <= 100)  #Using one of the comparison operators in Python, write a simple two-line program
+print(n > 100)  #Using one of the comparison operators in Python, write a simple two-line program
 # that takes the parameter n as input, which is an integer, and prints False if n is less than 100, 
 # and True if n is greater than or equal to 100.
