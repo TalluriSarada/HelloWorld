f7f0f32233dc34e0c8125cd113a2746928439a63
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 17:39:09 -0600

    braking the loop

---
+++ breakstatement.py
@@ -0,0 +1,6 @@
+while True:
+      secret_word = input("You 're stuck in an infinite loop!\nEnter a secret word to leave the loop.")
+      if secret_word == "chupacabra":
+          break
+print("You've successfully left the loop.") 
+#The break statement is used to exit/terminate a loop.
