5620a549a7ac2a66af0ea64813c2841074777b11
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 16:39:02 -0600

    guess the secret number

---
+++ secret_number.py
@@ -0,0 +1,8 @@
+secret_number = 777
+guessNumber = int(input("What is your guess"))
+while guessNumber != secret_number:
+      print("Ha ha: You're stuck in my loop!*")
+      guess =int(input("what is your guess?"))
+      print("Well done, muggle you re free now")
+
+#Guess the secret number game
