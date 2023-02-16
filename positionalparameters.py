739820dbdddda35ccc449a9b8de74e7b72a4781d
SaradaTalluri <bonavisarada@gmail.com>
2023-02-16 12:58:09 -0600

    Positional Parameters

---
+++ positionalparameters.py
@@ -0,0 +1,13 @@
+def introduction(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+
+introduction("Luke", "Skywalker")
+introduction("Jesse", "Quick")
+introduction("Clark", "Kent")
+
+def introduction(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+
+introduction("Skywalker", "Luke")
+introduction("Quick", "Jesse")
+introduction("Kent", "Clark")
