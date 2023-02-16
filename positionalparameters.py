042877da8c39aecb05c9800af38006240cd063fd
SaradaTalluri <bonavisarada@gmail.com>
2023-02-16 13:04:35 -0600

    functions

--- positionalparameters.py
+++ positionalparameters.py
@@ -11,3 +11,34 @@ def introduction(first_name, last_name):
 introduction("Skywalker", "Luke")
 introduction("Quick", "Jesse")
 introduction("Kent", "Clark")
+
+# parameterized Functions
+def message(what, number):
+    print("Enter", what, "number", number)
+
+# invoke the function
+message("telephone", 11)
+message("price", 5)
+message("number", "number")
+#parameters live inside functions (this is their natural environment)
+#arguments exist outside functions, and are carriers of values passed to
+# A function is a block of code that performs a specific task when the function is called (invoked).
+#built-in functions which are an integral part of Python (such as the) print() input()
+#user-defined functions which are written by users for users  lambda
+def hello(name):    # defining a function
+    print("Hello,", name)    # body of the function
+
+
+name = input("Enter your name: ")
+
+hello(name)    # calling the function
+#name error
+hi()
+
+def hi():
+    print("hi!")
+    #type error
+    def hi():
+    print("hi")
+
+hi(5)
