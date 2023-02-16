e937b71f30c785d4ca01d98f1dc178cc554f60cc
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 20:44:17 -0600

    multiple exercises

--- removerepetation.py
+++ removerepetation.py
@@ -7,3 +7,46 @@ for i in my_list:
 print("The list with unique elements only:")
 print(my_list)
 #write a program which removes all the number repetitions from the list
+
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+
+del list_1[0]
+del list_2[0]
+
+print(list_3)
+
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+
+del list_1[0]
+del list_2
+
+print(list_3)
+
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+
+del list_1[0]
+del list_2[:]
+
+print(list_3)
+
+list_1 = ["A", "B", "C"]
+list_2 = list_1[:]
+list_3 = list_2[:]
+
+del list_1[0]
+del list_2[0]
+
+print(list_3)
+
+my_list = [1, 2, "in", True, "ABC"]
+
+print(1 in my_list)  # outputs True
+print("A" in my_list)  # outputs True
+print(3  in my_list)  # outputs True
+print(False in my_list)  # outputs False
