64912bb4388cfdd44df76ea4135b32a226a16a14
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 18:54:24 -0600

    Binary left shift and right shiftoperators

---
+++ shiftoperators.py
@@ -0,0 +1,9 @@
+var = 17
+var_right = var >> 1
+var_left = var << 2
+print(var, var_left, var_right)
+#Binary left shift and binary right shift 
+# 17 >>1 (17 floor-divided by 2 to the power of 1) =8
+# (shifting to the right by one bit is the same as integer division by two)
+# 17 <<2  17 multiplied by 2 to the power of 2 =68
+# (shifting to the left by two bits is the same as integer multiplication by four
