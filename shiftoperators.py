515bf3b5db27610664d0b4849e05620581cfb6ea
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 19:01:04 -0600

    shiftoperators

--- shiftoperators.py
+++ shiftoperators.py
@@ -7,3 +7,15 @@ print(var, var_left, var_right)
 # (shifting to the right by one bit is the same as integer division by two)
 # 17 <<2  17 multiplied by 2 to the power of 2 =68
 # (shifting to the left by two bits is the same as integer multiplication by four
+x = 4
+y = 1
+
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+
+print(a, b, c, d, e, f)
+#logical operator
