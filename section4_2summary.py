0866d01c9a354d829091afde339690a4948f284c
SaradaTalluri <bonavisarada@gmail.com>
2023-02-16 13:17:37 -0600

    section summary

---
+++ section4_2summary.py
@@ -0,0 +1,19 @@
+def intro(a="James Bond", b="Bond"):
+    print("My name is", b + ".", a + ".")
+
+intro()
+
+def intro(a="James Bond", b="Bond"):
+    print("My name is", b + ".", a + ".")
+
+intro(b="Sean Connery")
+
+def intro(a, b="Bond"):
+    print("My name is", b + ".", a + ".")
+
+intro("Susan")
+#syntax error
+def add_numbers(a, b=2, c):
+    print(a + b + c)
+
+add_numbers(a=1, c=3)
