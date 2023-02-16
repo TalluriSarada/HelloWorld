92d4a8c6f39719b132bb9d78f8a141e4f6b9f4b9
SaradaTalluri <bonavisarada@gmail.com>
2023-02-15 19:01:31 -0600

    lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+numbers = [10, 5, 7, 2, 1]
+print("List content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("\nPrevious list content:", numbers)  # Printing previous list content.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("New list content:", numbers)  # Printing current list content.
+#Indexing lists
