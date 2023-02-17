fbd629b9d82e1102600d512b64b3783a6c158998
SaradaTalluri <bonavisarada@gmail.com>
2023-02-16 18:40:52 -0600

    key and values

--- howtousedictionary.py
+++ howtousedictionary.py
@@ -10,3 +10,22 @@ for word in words:
         print(word, "->", dictionary[word])
     else:
         print(word, "is not in dictionary")
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+#for key in dictionary.keys():
+for key in sorted(dictionary.keys()):
+    print(key, "->", dictionary[key])
+#How to use a dictionary: the keys()
+
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+for english, french in dictionary.items():
+    print(english, "->", french)
+
+#There is also a method named values(), which works similarly to keys(), 
+#but returns values.
+dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+for english, french in dictionary.items():
+    print(english, "->", french)
