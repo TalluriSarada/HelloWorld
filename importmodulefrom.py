595dab0fb9f62d341c93afcd87ef2eabb037e3b4
SaradaTalluri <bonavisarada@gmail.com>
2023-02-17 13:17:01 -0600

    importmodulefrom.py

---
+++ importmodulefrom.py
@@ -0,0 +1,38 @@
+from math import sin, pi
+
+print(sin(pi / 2))
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+#output display change --through import module at end
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+
+from math import sin, pi
+
+print(sin(pi / 2))
+
+import math
+
+for name in dir(math):
+    print(name, end="\t")
+
