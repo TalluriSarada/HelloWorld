4b63c1908e69d8e353f0dd29349062dfeaa251aa
SaradaTalluri <bonavisarada@gmail.com>
2023-02-20 19:48:59 -0600

    double

--- selectedfunctionfromthemodule.py
+++ selectedfunctionfromthemodule.py
@@ -8,3 +8,21 @@ print(ad == 90.)
 print(ar == pi / 2.)
 print(sin(ar) / cos(ar) == tan(ar))
 print(asin(sin(ar)) == ar)
+
+from math import e, exp, log
+
+print(pow(e, 1) == exp(log(e)))
+print(pow(2, 2) == exp(2 * log(2)))
+print(log(e, e) == exp(0))
+
+from math import ceil, floor, trunc
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
