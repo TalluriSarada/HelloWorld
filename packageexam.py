39eb34143baa27996a9f2d34211df628b2ec428b
Sarada Talluri <124937457+TalluriSarada@users.noreply.github.com>
2023-03-19 15:23:33 -0500

    ModulesandPackages

---
+++ packageexam.py
@@ -0,0 +1,9 @@
+from sys import path
+
+#path.append('\extra\good\best')
+import extra.good.best.sigma
+#from extra.good.best.tau import funT
+
+print(extra.good.best.sigma.funS())
+#print(funT())
+
